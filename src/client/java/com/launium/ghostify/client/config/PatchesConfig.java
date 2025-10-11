8db2ede67b19be7f55df4467773594e17bf4dfaf
Launium <admin@launium.com>
2025-10-12 03:51:08 +0800

    Lite Patches

--- src/client/java/com/launium/ghostify/client/config/PatchesConfig.java
+++ src/client/java/com/launium/ghostify/client/config/PatchesConfig.java
@@ -22,12 +22,6 @@ public class PatchesConfig extends AbstractConfig {
     @SerializedName("cancel_shortbow_pull")
     public boolean CANCEL_SHORTBOW_PULL = true;
 
-    @SerializedName("fix_dungeon_block_place")
-    public boolean FIX_DUNGEON_BLOCK_PLACE = false;
-
     @SerializedName("no_command_execution_confirmation")
     public boolean NO_COMMAND_EXECUTION_CONFIRMATION = false;
-
-    @SerializedName("overrule_skyblocker_glow_depth_test")
-    public boolean OVERRULE_SKYBLOCKER_GLOW_DEPTH_TEST = true;
 }
